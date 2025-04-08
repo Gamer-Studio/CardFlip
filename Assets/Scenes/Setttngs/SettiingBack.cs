5ac901fdbbdb771a851fa108575ee1f08ae018ab
피타쵸쵸 <djfb123@naver.com>
2025-04-08 19:23:50 +0900

    일시정지  수정
    - 일시정지 메뉴중 '사용자 설정'을  '설정'으로 변경.
    - 일시정지 메뉴에서 설정을 누르면 오류가 생겨 로드 되지 않는 문제
     유니티에서  씬 추가로 해결(Ctrl + shift + B 이후 드래그 드롭 )

---
+++ Assets/Scenes/Setttngs/SettiingBack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace CardFlip
+{
+    public class SettiingBack : MonoBehaviour
+    {
+        public void Back()
+        {   
+            SceneManager.LoadScene("SettingScene", LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("SettingScene");
+
+            SceneManager.LoadScene("MenuScene", LoadSceneMode.Additive);
+            
+        }
+    }
+}
