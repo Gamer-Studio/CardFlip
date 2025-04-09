53add9a3ff85e30b0b456c148ddc9424a92562e0
AmteurPotion <1k0j2i4@gmail.com>
2025-04-09 14:20:15 +0900

    GameManager 싱글톤을 GameScene에서 종속성 해제

--- Assets/Scenes/Game/Board.cs
+++ Assets/Scenes/Game/Board.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
@@ -10,17 +13,26 @@ namespace CardFlip
   public class Board : MonoBehaviour
   {
     public static Board Instance { get; private set; }
-    public AssetReference rtanRef;
+    public AssetLabelReference rtanRef;
     public Dictionary<string, Sprite> sprites = new();
     public RuleTile baseTile;
+    public Card selectedCard = null;
+    public int cardCount = 0;
+    [SerializeField]
+    private new Camera camera;
     [SerializeField]
     private Tilemap grid;
-    int cardOrder = 0;
+    [SerializeField]
+    private GameObject endText;
+    [SerializeField]
+    private TextMeshProUGUI timeText;
+    private int cardOrder = 0;
+    private float time = 30.0f;
     private void Awake()
     {
       if (Instance == null) Instance = this;
 
-      var loadSprites = Addressables.LoadAssetsAsync<Sprite>("rtan", sprite =>
+      var loadSprites = Addressables.LoadAssetsAsync<Sprite>(rtanRef, sprite =>
       {
         sprites.Add(sprite.name, sprite);
       }).WaitForCompletion();
@@ -29,6 +41,63 @@ namespace CardFlip
     private void Start()
     {
       StartGame();
+    }
+
+    private void Update()
+    {
+      if (time > 0) // 0초가 아닐 때
+      {
+        time -= Time.deltaTime; // 시간 감소
+        timeText.text = time.ToString("N2");
+      }
+      else
+      {
+        timeText.text = 0f.ToString("N2"); // 끝나면 화면에 0초로 표시
+        GameOver();
+      }
+    }
+
+    private void OnClick(InputValue value)
+    {
+      if (value.Get<float>() == 1)
+      {
+        var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+        var hits = Physics2D.RaycastAll(pos, Vector2.zero, 0f);
+
+        foreach (var hit in hits)
+        {
+          if (hit.collider.TryGetComponent<Card>(out var card) && card != selectedCard && !card.destroy)
+          {
+            var anim = card.GetComponent<Animator>();
+            anim.SetTrigger("Open");
+            anim.SetBool("isOpen", true);
+
+            // 카드 선택했을 때 동작
+            if (selectedCard == null) selectedCard = card;
+            else
+            {
+              if (selectedCard.Id == card.Id)
+              {
+                anim.SetBool("destroy", true);
+                selectedCard.GetComponent<Animator>().SetBool("destroy", true);
+                cardCount -= 2;
+                // match
+              }
+              else
+              {
+                selectedCard.GetComponent<Animator>().SetBool("isOpen", false);
+                anim.SetBool("isOpen", false);
+
+              }
+              selectedCard = null;
+            }
+
+            break;
+          }
+        }
+
+        if (cardCount <= 0) GameOver();
+      }
     }
 
     private void StartGame()
@@ -50,7 +119,19 @@ namespace CardFlip
         }
       }
 
-      GameManager.Instance.cardCount = arr.Length;
+      cardCount = arr.Length;
+    }
+
+    public void GameOver()
+    {
+      endText.SetActive(true);
+      Time.timeScale = 0;
+    }
+
+    public void Retry()
+    {
+      Time.timeScale = 1;
+      SceneManager.LoadScene("MainScene");
     }
 
     public Card SetCard(int id, Vector3Int position)
