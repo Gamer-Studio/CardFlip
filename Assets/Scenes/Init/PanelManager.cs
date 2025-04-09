14cc3805a7dae8d2e20298ac5588235898510f9b
AmteurPotion <1k0j2i4@gmail.com>
2025-04-09 14:17:46 +0900

    일시정지 패널 구현

---
+++ Assets/Scenes/Init/PanelManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace CardFlip
+{
+  public enum PanelType
+  {
+    Setting = 1,
+    Pause = 2
+  }
+  public class PanelManager : MonoBehaviour
+  {
+    public static PanelManager Instance { get; private set; } = null;
+    public UnityEvent<PanelType, bool> OnOpen;
+    [SerializeField]
+    private GameObject pausePanel, settingPanel;
+    public bool OpenPause
+    {
+      get => pausePanel.activeSelf;
+      set
+      {
+        pausePanel.SetActive(value);
+        OnOpen.Invoke(PanelType.Pause, value);
+      }
+    }
+    public bool OpenSetting
+    {
+      get => settingPanel.activeSelf;
+      set
+      {
+        settingPanel.SetActive(value);
+        OnOpen.Invoke(PanelType.Setting, value);
+      }
+    }
+
+    private void Awake()
+    {
+      if (Instance == null) Instance = this;
+      DontDestroyOnLoad(gameObject);
+
+      var canvas = GetComponent<Canvas>();
+
+      SceneManager.sceneLoaded += (_, _) =>
+      {
+        canvas.worldCamera = Camera.main;
+      };
+    }
+
+    private void Start()
+    {
+      OpenPause = true;
+    }
+  }
+}
