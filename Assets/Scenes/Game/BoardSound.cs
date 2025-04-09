fe46f03b0ad2f82704c34526ae40483ee105305f
AmteurPotion <1k0j2i4@gmail.com>
2025-04-09 15:01:58 +0900

    OnClick 메소드 이동

--- Assets/Scenes/Game/BoardSound.cs
+++ Assets/Scenes/Game/BoardSound.cs
@@ -1,6 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace CardFlip
 {
   public partial class Board
   {
+    private void OnClick(InputValue value)
+    {
+      if (value.Get<float>() == 1)
+      {
+        var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+        var hits = Physics2D.RaycastAll(pos, Vector2.zero, 0f);
+
+        foreach (var hit in hits)
+        {
+          if (hit.collider.TryGetComponent<Card>(out var card) && card != selectedCard && !card.destroy)
+          {
+            var anim = card.GetComponent<Animator>();
+            anim.SetTrigger("Open");
+            anim.SetBool("isOpen", true);
+
+            // 카드 선택했을 때 동작
+            if (selectedCard == null) selectedCard = card;
+            else
+            {
+              if (selectedCard.Id == card.Id)
+              {
+                anim.SetBool("destroy", true);
+                selectedCard.GetComponent<Animator>().SetBool("destroy", true);
+                cardCount -= 2;
+                // match
+              }
+              else
+              {
+                selectedCard.GetComponent<Animator>().SetBool("isOpen", false);
+                anim.SetBool("isOpen", false);
+
+              }
+              selectedCard = null;
+            }
+
+            break;
+          }
+        }
+
+        if (cardCount <= 0) GameOver();
+      }
+    }
   }
 }
