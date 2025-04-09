41f35c6d08129e6e922043878078e1795ecc427a
JeonCM <wsd3576@gmail.com>
2025-04-09 10:47:56 +0900

    카드 배치 애니메이션 추가
    -보드 스크립트 카드 순서 변수 추가 및 카드 순서 지정 코드 추가
    -카드 스크립트 25~50까지 새 코드 추가

--- Assets/Scenes/Game/Card.cs
+++ Assets/Scenes/Game/Card.cs
@@ -1,24 +1,52 @@
 using JetBrains.Annotations;
 using UnityEngine;
+using System.Collections;
+
 
 namespace CardFlip
 {
-  public class Card : MonoBehaviour
-  {
-    public int Id
+    public class Card : MonoBehaviour
     {
-      get => id;
-      set
-      {
-        renderer.sprite = Board.Instance.sprites[$"rtan{value}"];
-        id = value;
-      }
+        public int Id
+        {
+            get => id;
+            set
+            {
+                renderer.sprite = Board.Instance.sprites[$"rtan{value}"];
+                id = value;
+            }
+        }
+        public bool destroy = false;
+        private int id = 0;
+        [SerializeField]
+        private new SpriteRenderer renderer;
+        [SerializeField]
+        private Animator animator;
+
+        public int order = 0;
+        private Vector3 pos;
+        void Start()
+        {
+            pos = transform.position;
+            transform.position = Vector3.zero;
+            StartCoroutine(MoveToTarget());
+        }
+
+        IEnumerator MoveToTarget()
+        {
+            float elapsed = 0f;
+            float delay = order * 0.2f;
+            yield return new WaitForSeconds(delay);
+
+            while (elapsed < 0.3f)
+            {
+                transform.position = Vector3.Lerp(Vector3.zero, pos, elapsed / 0.3f);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = pos;
+            animator.SetBool("isDeployed", true);
+        }
     }
-    public bool destroy = false;
-    private int id = 0;
-    [SerializeField]
-    private new SpriteRenderer renderer;
-    [SerializeField]
-    private Animator animator;
-  }
 }
