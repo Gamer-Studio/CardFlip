badcb599f44bd2863a16b0f645b29d6655d16c29
AmteurPotion <1k0j2i4@gmail.com>
2025-04-10 10:19:39 +0900

    메뉴 씬 스크립트 통함중

---
+++ Assets/Scenes/Menu/MenuSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardFlip
+{
+  public class MenuSystem : MonoBehaviour
+  {
+    public void SelectStage()
+    {
+
+    }
+
+    public void OpenSetting()
+    {
+      PanelManager.Instance.OpenSetting = true;
+    }
+
+    public void ExitGame()
+    {
+
+    }
+  }
+}
